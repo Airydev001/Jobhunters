5c21a2b1d1421c223d79d6b302f09dccd393a6df
Airydev001 <josysmith02@gmail.com>
2023-09-16 11:27:53 +0100

    great

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ mongoose.connect(`mongodb+srv:${process.env.MONGO_URL}`).then(()=> console.log("
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
 
 
 app.use("/api/", authRoute);
@@ -29,6 +39,7 @@ app.use("/api/jobs", jobRoute);
 
 
 app.use("/api/bookmarks", bookRoute);
+
 
 
 
